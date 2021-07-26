d8fc9389531a7b1210eab594e569988ae697d4d7
Ludson Sacilotto <admvianett@gmail.com>
2021-07-26 15:06:04 -0300

    criando o page admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -14,6 +15,14 @@ $app->get('/', function() {
     	$page = new Page();// chama o CONSTRUCT
 
     	$page->setTpl("index"); //chama o INDEX
+
+});
+
+$app->get('/admin', function() {
+        
+        $page = new PageAdmin();// chama o CONSTRUCT
+
+        $page->setTpl("index"); //chama o INDEX
 
 });
 
