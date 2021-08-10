c8760cd00059cf9fb58aa96e074169f0efd297da
Ludson Sacilotto <admvianett@gmail.com>
2021-08-10 09:26:41 -0300

    Site alterar cadastro

--- site.php
+++ site.php
@@ -307,7 +307,65 @@ $app->post("/forgot/reset", function(){
 		$page = new Page();
 
 		$page->setTpl("forgot-reset-success");
+});
 
+$app->get("/profile", function(){
+
+	User::verifyLogin(false);
+
+	$user = User::getFromSession();
+
+	$page = new Page();
+
+	$page->setTpl("profile", [
+		'user'=>$user->getvalues(),
+		'profileMsg'=>User::getSuccess(),
+		'profileError'=>User::getError()
+	]);	
+
+});
+
+$app->post("/profile", function(){
+
+	User::verifyLogin(false);
+
+	if (!isset($_POST['desperson']) || $_POST['desperson'] === '') {
+		User::setError("Preencha seu nome");
+		header('Location: /profile');
+		exit;
+	}
+
+	if (!isset($_POST['desemail']) || $_POST['desemail'] === '') {
+		User::setError("Preencha seu e-mail.");
+		header('Location: /profile');
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	if ($_POST['desemail'] !== $user->getdesemail()) {
+
+		if (User::checkLoginExists($_POST['desemail']) === true) {
+
+			User::setError("Este endereço de e-mails já está cadastrado");
+			header('Location: /profile');
+			exit;
+		}
+
+	}
+
+	$_POST['inadmin'] = $user->getinadmin();
+	$_POST['despassword'] = $user->getdespassword();
+	$_POST['deslogin'] = $_POST['desemail'];
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	User::setSuccess("Dados alterados com sucesso");
+
+	header('Location: /profile');
+	exit;
 });
 
 ?>
